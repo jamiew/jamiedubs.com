566b85d38dbbef67e34e2e325ab02a4066a155c7
Jamie Wilkinson <jamie@jamiedubs.com>
2021-07-27 23:04:59 -0400

    Revert "HNT oracle post"
    This reverts commit 9e935576e90d4bdf2710947eb03d73e5e78e1eec.

--- _posts/sync-obsidian.sh
+++ _posts/sync-obsidian.sh
@@ -19,7 +19,6 @@ copy "2021-05-30 - A brief history of PartyDAO.md" ./2021-05-29-a-brief-history-
 copy "2021-06-01 - How to submit a HIP or Helium manufacturer application.md" ./2021-06-01-how-to-submit-helium-manufacturer-application.md
 copy "2021-06-02 - PartyDAO is what startups will look like in the future.md" ./2021-06-02-partydao-what-startups-will-look-like-in-future.md
 copy "2021-07-13 - Breaking down the Songcamp Elektra crowdfund.md" ./2021-07-13-breaking-down-songcamp-elektra-crowdfund.md
-copy "2021-07-27 - How to be an HNT price oracle.md" ./2021-07-27-how-to-be-an-hnt-price-oracle.md
 
 # git commit -a -m "Update post"
 
